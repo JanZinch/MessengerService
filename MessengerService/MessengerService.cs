290e7d5543cacee94202603048ebbbca1f47611b
JanZinch <jan.zinchanka@mail.ru>
2023-11-10 17:45:34 +0300

    Add project files.

---
+++ MessengerService/MessengerService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessengerService
+{
+    public partial class MessengerService : ServiceBase
+    {
+        public MessengerService()
+        {
+            InitializeComponent();
+
+            this.CanStop = true;
+            this.CanPauseAndContinue = true;
+            this.AutoLog = true;
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            Console.WriteLine("Service initialized");
+        }
+
+        protected override void OnStop()
+        {
+            Console.WriteLine("Service stopped");
+        }
+    }
+}
---
+++ MessengerService/MessengerServiceInstaller.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Linq;
+using System.ServiceProcess;
+using System.Threading.Tasks;
+
+namespace MessengerService
+{
+    [RunInstaller(true)]
+    public partial class MessengerServiceInstaller : System.Configuration.Install.Installer
+    {
+        ServiceInstaller _serviceInstaller;
+        ServiceProcessInstaller _processInstaller;
+
+        public MessengerServiceInstaller()
+        {
+            InitializeComponent();
+            _serviceInstaller = new ServiceInstaller();
+            _processInstaller = new ServiceProcessInstaller();
+
+            _serviceInstaller.StartType = ServiceStartMode.Manual;
+            _serviceInstaller.ServiceName = "MessengerService";
+            _processInstaller.Account = ServiceAccount.LocalSystem;
+
+            Installers.Add(_processInstaller);
+            Installers.Add(_serviceInstaller);
+
+
+        }
+    }
+}
